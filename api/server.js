9cc0ac0901b49501a0cbba2051b60313ae9bd32c
Nagarjun <deepaknagarjun@gmail.com>
2021-07-04 13:16:12 +0530

    changes for bot - init

--- api/server.js
+++ api/server.js
@@ -35,6 +35,23 @@
 // module.exports = bot;
 
 module.exports = (req, res) => {
-    const { name = 'World' } = req.query;
-    res.status(200).send(`Hello ${name}`);
+    const telegramText = req.body
+    && req.body.message
+    && req.body.message.chat
+    && req.body.message.chat.id
+    && req.body.message.from
+    && req.body.message.from.first_name
+
+    if (telegramText) {
+        const chat_id = req.body.message.chat.id
+        const first_name = req.body.message.from.first_name
+        const receivedMessage = req.body.message.text
+
+        return res.status(200).send({
+            method: 'sendMessage',
+            chat_id,
+            text: `Hello ${first_name}, \n You sent us message: ${receivedMessage}`
+        })
+    }
+    return res.status(200).send({status: 'An error occurred'})
 };
